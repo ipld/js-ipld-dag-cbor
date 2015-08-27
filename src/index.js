8ca67da83384373a365fe8193a813175ace4742a
David Dias <daviddias.p@gmail.com>
2015-08-27 13:31:13 +0100

    always use latest schemas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+var fs = require('fs')
+var path = require('path')
+
 exports = module.exports
 
+var versions = fs.readFileSync(path.resolve(__dirname, '../versions'), 'utf8').split('\n')
+
+console.log(versions[versions.length - 2])
+
 exports.context = {
-  merkleweb: 'http://gateway.ipfs.io/ipfs/QmbuH1ZExsQvzVEFFw9S2CivEkHrQ9KmCy6zbxSymq8X5r/merkleweb'
+  merkleweb: 'http://gateway.ipfs.io/ipfs/' + versions[versions.length - 2] + '/merkleweb'
 }
 
 exports.types = {
