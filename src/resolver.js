0b6460ba47c4fb238ba80a126f3e110e5d0ec054
Mitar <mitar.git@tnode.com>
2017-02-24 13:34:29 -0800

    Converted to sync interface.

--- src/resolver.js
+++ src/resolver.js
@@ -11,90 +11,70 @@ exports.multicodec = 'dag-cbor'
  * resolve: receives a path and a block and returns the value on path,
  * throw if not possible. `block` is an IPFS Block instance (contains data + key)
  */
-exports.resolve = (block, path, callback) => {
-  if (typeof path === 'function') {
-    callback = path
-    path = undefined
+exports.resolve = (block, path) => {
+  let node = util.deserialize(block.data)
+
+  // root
+
+  if (!path || path === '/') {
+    return {
+      value: node,
+      remainderPath: ''
+    }
   }
 
-  util.deserialize(block.data, (err, node) => {
-    if (err) {
-      return callback(err)
+  // within scope
+
+  // const tree = exports.tree(block)
+  const parts = path.split('/')
+  const val = traverse(node).get(parts)
+
+  if (val) {
+    return {
+      value: val,
+      remainderPath: ''
     }
+  }
 
-    // root
+  // out of scope
+  let value
+  let len = parts.length
 
-    if (!path || path === '/') {
-      return callback(null, {
-        value: node,
-        remainderPath: ''
-      })
-    }
+  for (let i = 0; i < len; i++) {
+    const partialPath = parts.shift()
 
-    // within scope
-
-    // const tree = exports.tree(block)
-    const parts = path.split('/')
-    const val = traverse(node).get(parts)
-
-    if (val) {
-      return callback(null, {
-        value: val,
-        remainderPath: ''
-      })
-    }
-
-    // out of scope
-    let value
-    let len = parts.length
-
-    for (let i = 0; i < len; i++) {
-      const partialPath = parts.shift()
-
-      if (Array.isArray(node) && !Buffer.isBuffer(node)) {
-        value = node[Number(partialPath)]
-      } if (node[partialPath]) {
-        value = node[partialPath]
+    if (Array.isArray(node) && !Buffer.isBuffer(node)) {
+      value = node[Number(partialPath)]
+    } if (node[partialPath]) {
+      value = node[partialPath]
+    } else {
+      // can't traverse more
+      if (!value) {
+        throw new Error('path not available at root')
       } else {
-        // can't traverse more
-        if (!value) {
-          return callback(new Error('path not available at root'))
-        } else {
-          parts.unshift(partialPath)
-          return callback(null, {
-            value: value,
-            remainderPath: parts.join('/')
-          })
+        parts.unshift(partialPath)
+        return {
+          value: value,
+          remainderPath: parts.join('/')
         }
       }
-      node = value
     }
-  })
+    node = value
+  }
 }
 
 /*
  * tree: returns a flattened array with paths: values of the project. options
  * are option (i.e. nestness)
  */
-exports.tree = (block, options, callback) => {
-  if (typeof options === 'function') {
-    callback = options
-    options = undefined
-  }
-
+exports.tree = (block, options) => {
   if (!options) {
     options = {}
   }
 
-  util.deserialize(block.data, (err, node) => {
-    if (err) {
-      return callback(err)
-    }
-    const flat = flattenObject(node)
-    const paths = flat.map((el) => el.path)
-
-    callback(null, paths)
-  })
+  const node = util.deserialize(block.data)
+  const flat = flattenObject(node)
+  return flat.map((el) => el.path)
 }
 
 function flattenObject (obj, delimiter) {
