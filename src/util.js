1ec7744a9fa0c99bc07b356c8924a8acf1e81c3f
Richard Schneider <makaretu@gmail.com>
2018-06-26 20:53:41 +1200

    fix: pass serialized blob to util.cid (#67)
    BREAKING CHANGE: the first argument is now the serialized output NOT the dag node.
    See https://github.com/ipld/interface-ipld-format/issues/32

--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const assert = require('assert')
 const cbor = require('borc')
 const multihashing = require('multihashing-async')
 const CID = require('cids')
@@ -116,14 +117,15 @@ exports.deserialize = (data, callback) => {
 /**
  * Get the CID of the DAG-Node.
  *
- * @param {Object} dagNode - Internal representation
+ * @param {Buffer} blob - Serialized binary data
  * @param {Object} [options] - Options to create the CID
  * @param {number} [options.version=1] - CID version number
  * @param {string} [options.hashAlg] - Defaults to hashAlg for the resolver
  * @param {CidCallback} callback - Callback that handles the return value
  * @returns {void}
  */
-exports.cid = (dagNode, options, callback) => {
+exports.cid = (blob, options, callback) => {
+  assert(Buffer.isBuffer(blob), 'blob must be a Buffer')
   if (typeof options === 'function') {
     callback = options
     options = {}
@@ -132,8 +134,7 @@ exports.cid = (dagNode, options, callback) => {
   const hashAlg = options.hashAlg || resolver.defaultHashAlg
   const version = typeof options.version === 'undefined' ? 1 : options.version
   waterfall([
-    (cb) => exports.serialize(dagNode, cb),
-    (serialized, cb) => multihashing(serialized, hashAlg, cb),
+    (cb) => multihashing(blob, hashAlg, cb),
     (mh, cb) => cb(null, new CID(version, resolver.multicodec, mh))
   ], callback)
 }
