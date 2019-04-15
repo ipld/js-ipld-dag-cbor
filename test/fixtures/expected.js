45e4ff12bbd85a38228984afdc2fdd86fd220f5d
Volker Mische <volker.mische@gmail.com>
2019-04-15 13:21:48 +0200

    test: make things more JS-y
    Use cample-case property names instead of names with dashed.

--- test/fixtures/expected.js
+++ test/fixtures/expected.js
@@ -3,10 +3,10 @@
 const CID = require('cids')
 
 module.exports = {
-  'array-link': new CID('zdpuAsZ8roJvCazJFXUnSULspbXfxrLkUkNxy6SToaiDpEiap'),
-  'empty-array': new CID('zdpuAtQy7GSHNcZxdBfmtowdL1d2WAFjJBwb6WAEfFJ6T4Gbi'),
-  'empty-obj': new CID('zdpuAyTBnYSugBZhqJuLsNpzjmAjSmxDqBbtAqXMtsvxiN2v3'),
+  arrayLink: new CID('zdpuAsZ8roJvCazJFXUnSULspbXfxrLkUkNxy6SToaiDpEiap'),
+  emptyArray: new CID('zdpuAtQy7GSHNcZxdBfmtowdL1d2WAFjJBwb6WAEfFJ6T4Gbi'),
+  emptyObj: new CID('zdpuAyTBnYSugBZhqJuLsNpzjmAjSmxDqBbtAqXMtsvxiN2v3'),
   foo: new CID('zdpuApbZ3Kmyxwa5rZ3mKHtQKkdHLKwHCXHTLbDSfEDfwkmen'),
-  'obj-with-link': new CID('zdpuAp1WRqfq8w953Dep9pz7nNYUbQPKX9duW71G5C4doiPFK'),
-  'obj-no-link': new CID('zdpuAtHFXfzgXBSoq5GSn3NTrK2ToYwj4tRZiMTfe2mVApYLa')
+  objWithLink: new CID('zdpuAp1WRqfq8w953Dep9pz7nNYUbQPKX9duW71G5C4doiPFK'),
+  objNoLink: new CID('zdpuAtHFXfzgXBSoq5GSn3NTrK2ToYwj4tRZiMTfe2mVApYLa')
 }
